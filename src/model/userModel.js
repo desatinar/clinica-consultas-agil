e8fe59cb3e171f81de68aab7aa9b8f6672ca6c7c
Rodrigo Santos <rodrigosdn@gmail.com>
2024-06-19 22:24:40 -0300

    alterada a forma de criar paciente para ter um array de consultas

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,19 +1,27 @@
 import { readDatabase, saveDatabase } from "./databaseModel.js";
 
-export function registerPatient(user){
+export function registerPatient(user) {
     const data = readDatabase();
     const userId = (data.users.length + 1).toString();
-    const newUser = {...user, id: userId, appointment: {date: null, time: null, specialty: null}};
-    data.users.push(newUser);
+    const newUser = {
+        ...user, 
+        id: userId, 
+        appointments: [
+            {
+                date: null, time: null, specialty: null
+            }
+        ],
+    }
 
+    data.users.push(newUser);
     saveDatabase(data);
 }
 
-export function isPatientRegistered(phone){
+export function isPatientRegistered(phone) {
     const database = readDatabase();
     const duplicatePhoneCheck = database.users.find(user => user.phone === phone);
 
-    if(duplicatePhoneCheck){
+    if (duplicatePhoneCheck) {
         return true;
     } else {
         return false;
